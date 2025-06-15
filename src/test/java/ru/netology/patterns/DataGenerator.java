8892d090b2412042f76639ab99f304816392d1ae
Kate Bakhtina <almazniegori@gmail.com>
2025-06-15 17:37:44 +0300

    create DataGenerator class

--- src/test/java/ru/netology/patterns/DataGenerator.java
+++ src/test/java/ru/netology/patterns/DataGenerator.java
@@ -2,37 +2,45 @@ package ru.netology.patterns;
 
 import com.github.javafaker.Faker;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataGenerator {
-    String city;
-    String date;
-    String name;
-    String phone;
-    Faker faker;
 
     public DataGenerator() {
-        this.city = generateCity();
-        this.date = generateDate();
-        this.name = generateName();
-        this.phone = generatePhone();
-        this.faker = new Faker(new Locale("ru"));
-
     }
 
-    private String generateCity() {
-        return "";
+    public class User {
+        Faker faker;
+        String name;
+        String city;
+        String phone;
+
+        public User() {
+            this.faker = new Faker(new Locale("ru"));
+
+            this.name = generateName();
+            this.city = generateCity();
+            this.phone = generatePhone();
+        }
+
+        private String generateName() {
+            return this.faker.name().firstName() + " " + this.faker.name().lastName();
+        }
+
+        private String generateCity() {
+            return this.faker.address().city();
+        }
+
+        private String generatePhone() {
+            return "+7" + this.faker.phoneNumber().subscriberNumber(10);
+        }
     }
 
-    private String generateDate() {
-        return "";
-    }
-
-    private String generateName() {
-        return faker.name().fullName();
-    }
-
-    private String generatePhone() {
-        return "";
+    public String generateDate(int days, String formatOfDate) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(formatOfDate));
     }
 }
+
+
