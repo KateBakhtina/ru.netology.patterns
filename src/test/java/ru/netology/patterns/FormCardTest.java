0ddf5b864252cbfccd91e44804da8b8a1900222b
Kate Bakhtina <almazniegori@gmail.com>
2025-06-16 23:23:35 +0300

    correct FormCardTest class

--- src/test/java/ru/netology/patterns/FormCardTest.java
+++ src/test/java/ru/netology/patterns/FormCardTest.java
@@ -16,36 +16,29 @@ import static org.openqa.selenium.Keys.chord;
 
 public class FormCardTest {
 
+    public void pasteDate(SelenideElement form, DataGenerator data, int days) {
+        form.$("[data-test-id='date'] input")
+                .press(chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
+                .setValue(data.generateDate(days, "dd.MM.yyyy"));
+    }
+
     @Test
     void replanDateTest() {
         DataGenerator data = new DataGenerator();
+        DataGenerator.User user = data.new User();
 
         Selenide.open("http://0.0.0.0:9999");
         SelenideElement form = $("#root form");
-
-        form.$("[data-test-id='date'] input")
-                .press(chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
-                .setValue(data.generateDate(3, "dd.MM.yyyy"));
-
-        DataGenerator.User user = data.new User();
-
-        form.$("[data-test-id='name'] input").setValue(user.name);
         form.$("[data-test-id='city'] input").setValue(user.city);
+        pasteDate(form, data, 3);
+        form.$("[data-test-id='name'] input").setValue(user.name);
         form.$("[data-test-id='phone'] input").setValue(user.phone);
         form.$("[data-test-id='agreement']").click();
         form.$(withText("Запланировать")).click();
-
         $("#root [data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
-
-        form.$("[data-test-id='date'] input")
-                .press(chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE)
-                .setValue(data.generateDate(5, "dd.MM.yyyy"));
+        pasteDate(form, data, 5);
         form.$(withText("Запланировать")).click();
-
-        $("#root [data-test-id='replan-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
-
-        $("#root [data-test-id='replan-notification'] button").click();
-
+        $("#root [data-test-id='replan-notification'] button").shouldBe(Condition.visible, Duration.ofSeconds(10)).click();
         $("#root [data-test-id='success-notification']").shouldBe(Condition.visible, Duration.ofSeconds(10));
     }
 }
